bf12480365695eaf3d7eab2071fa8fb547fe0ae4
Siddharth Sisodia <139268988+siddharth2504@users.noreply.github.com>
2025-05-15 02:10:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 // Complete the js code
-function Car(make, model) {}
+// function Car(make, model) {}
 
-function SportsCar(make, model, topSpeed) {}
+// function SportsCar(make, model, topSpeed) {}
 
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+// // Do not change the code below
+// window.Car = Car;
+// window.SportsCar = SportsCar;
+
+class Car{
+	constructor(make, model){
+		this.make = make
+		this.model = model
+	}
+	getMakeModel(){
+		return `${this.make} ${this.model}`
+	}
+}
+
+class SportsCar extends Car{
+	constructor(make, model, topSpeed){
+		super(make, model)
+		this.topSpeed = topSpeed
+	}
+	getTopSpeed(){
+		return this.topSpeed
+	}
+}
+
+const car = new SportsCar("Ferrari", "Testarossa", 200)
+console.log(car.getMakeModel())
+console.log(car.getTopSpeed()) 
